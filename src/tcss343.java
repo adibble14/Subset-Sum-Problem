1ed09d218b192e7c116d4266f07d0da177cbbb95
Andrew Dibble <andrewdibble14@outlook.com>
2022-11-30 16:28:08 -0800

    adding the layout of methods

--- src/tcss343.java
+++ src/tcss343.java
@@ -1,2 +1,12 @@
 public class tcss343 {
+    public static void main(String[] args){}
+
+    public static void BruteForce(){}
+
+    public static void DynamicProgramming(){}
+
+    public static void CleverAlgorithm(){}
+
+    public static void Driver(){}
+
 }
