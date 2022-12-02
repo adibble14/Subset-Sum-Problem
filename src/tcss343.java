780bd11f0349b19322979f1fba76f68f7ac73261
Masayoki <Masayoki88@yahoo.com>
2022-12-02 00:22:52 -0800

    Working on Bruteforce algorithm

--- src/tcss343.java
+++ src/tcss343.java
@@ -1,7 +1,26 @@
 public class tcss343 {
-    public static void main(String[] args){}
-
-    public static void BruteForce(){}
+    
+    public static void main(String[] args) {
+        int[] set = {3, 34, 4, 12, 5, 2};
+        int n = set.length;
+        int target = 9;
+        if (BruteForce(set, n, target))
+            System.out.println("Found subset with given sum");
+        else
+            System.out.println("No subset with given sum");
+    }
+    public static boolean BruteForce(int[] target, int n, int sum) {
+        if (sum == 0) {
+            return true;
+        }
+        if (n == 0) {
+            return false;
+        }
+        if (target[n-1] > sum) {
+            return BruteForce(target, n-1, sum);
+        }
+        return BruteForce(target, n-1, sum) || BruteForce(target, n-1, sum-target[n-1]);
+    }
 
     public static void DynamicProgramming(){}
 
