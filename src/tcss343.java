9d6111796627385bc1212e5c4ec82f7f329a81e6
Masayoki <Masayoki88@yahoo.com>
2022-12-03 21:42:17 -0800

    adding bruteforce algorithm

--- src/tcss343.java
+++ src/tcss343.java
@@ -22,7 +22,25 @@ public class tcss343 {
         Driver(5, 5, false);
     }
 
-    public static void BruteForce() {
+    public static boolean BruteForce(int[] target, int n, int sum) {
+        //Our base cases
+        //Returns true if sum is equal to 0
+        if (sum == 0) {
+            return true;
+        }
+        //Returns true if n is equal to 0
+        if (n == 0) {
+            return false;
+        }
+        //Determining if our last element is greater than our sum
+        //and if so ignoring it
+        if (target[n - 1] > sum) {
+            return BruteForce(target, n - 1, sum);
+        }
+        //Else check if sum can be obtained by
+        //Including last element
+        //Excluding last element
+        return BruteForce(target, n - 1, sum) || BruteForce(target, n - 1, sum - target[n - 1]);
     }
 
     public static ArrayList<Object> dynamicProgramming(
